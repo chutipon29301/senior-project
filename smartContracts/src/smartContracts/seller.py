9c5d12011ad7e213690ca8e519d479afa4e067f4
mingyanisa <mingignim@hotmail.com>
2019-12-04 16:35:31 +0700

    :sparkles: add evaluation index to buyer seller

--- smartContracts/src/smartContracts/seller.py
+++ smartContracts/src/smartContracts/seller.py
@@ -4,13 +4,17 @@ class Seller:
     reservePrice = 0
     transaction=[]
     totalSoldPrice=0
+    utilityIndex=0
+    ssi=0;mti=0
     # default constructor 
-    def __init__(self,quantityAvailable,reservePrice): 
+    def __init__(self,quantityAvailable,reservePrice,quantityLeft=quantityAvailable): 
         self.quantityAvailable = quantityAvailable
-        self.quantityLeft = quantityAvailable
+        self.quantityLeft = quantityLeft
         self.reservePrice = reservePrice
         self.transaction=[]
         self.totalSoldPrice=0
+        self.utilityIndex=0
+        self.ssi=0;self.mti=0
   
     # a method for printing data members 
     def print_seller(self): 
@@ -18,9 +22,11 @@ class Seller:
         "\nreserve price:",self.reservePrice,
         "\nseller quantity left:",self.quantityLeft,
         "\nseller sold transaction:",self.transaction,
-        "\nseller totalSoldPrice price:",self.totalSoldPrice
+        "\nseller totalSoldPrice price:",self.totalSoldPrice, 
+        "\nssi:",self.ssi,
+        "\nmti:",self.mti,
         ) 
         # print("====")
         
-    def to_dict(self):
+    def toDict(self):
       return {"transaction": self.transaction, "totalSoldPrice": self.totalSoldPrice}
