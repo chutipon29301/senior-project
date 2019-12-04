b4cb3b17eff270055accf31d4122dbd48d86f528
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-04 14:54:59 +0700

    :sparkles: Add fabric-server smart contracts module

--- fabric-server/src/app.module.ts
+++ fabric-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SmartContractsModule } from './smart-contracts/smart-contracts.module';
 
 @Module({
-  imports: [],
+  imports: [SmartContractsModule],
   controllers: [AppController],
   providers: [AppService],
 })
