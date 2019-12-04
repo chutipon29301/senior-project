179cc9829ff42e85d6cb48fcafe4301607996c23
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-04 15:04:06 +0700

    :sparkles: Add fabric-server seller module

--- fabric-server/src/app.module.ts
+++ fabric-server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SmartContractsModule } from './smart-contracts/smart-contracts.module';
+import { BuyerModule } from './buyer/buyer.module';
+import { SellerModule } from './seller/seller.module';
 
 @Module({
-  imports: [SmartContractsModule],
+  imports: [SmartContractsModule, BuyerModule, SellerModule],
   controllers: [AppController],
   providers: [AppService],
 })
