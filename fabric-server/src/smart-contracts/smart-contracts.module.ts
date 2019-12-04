46dd49c540359a65e380bfd51527f94fd80cc085
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-04 16:11:01 +0700

    :sparkles: Add ping request to smart-contract module

--- fabric-server/src/smart-contracts/smart-contracts.module.ts
+++ fabric-server/src/smart-contracts/smart-contracts.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { SmartContractsService } from './smart-contracts.service';
 import { SmartContractsController } from './smart-contracts.controller';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
+  imports: [HttpModule, ConfigModule],
   providers: [SmartContractsService],
   controllers: [SmartContractsController],
 })
--- fabric-server/src/smart-contracts/smart-contracts.service.ts
+++ fabric-server/src/smart-contracts/smart-contracts.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { ConfigService } from '../config/config.service';
 
 @Injectable()
-export class SmartContractsService {}
+export class SmartContractsService {
+    constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) { }
+
+    public async ping(): Promise<string> {
+        const { data } = await this.httpService.get<string>(`http://${this.configService.smartContractAPIUrl}`).toPromise();
+        return data;
+    }
+}
