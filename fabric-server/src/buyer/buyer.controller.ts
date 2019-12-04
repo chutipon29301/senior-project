6370d1af24fbee62e61d3e15c62f99d1ad2e52ff
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-04 15:40:07 +0700

    :sparkles: Add request for buyer to offer price

--- fabric-server/src/buyer/buyer.controller.ts
+++ fabric-server/src/buyer/buyer.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Param, Body } from '@nestjs/common';
+import { Price } from './buyer.dto';
 
 @Controller('buyer')
-export class BuyerController {}
+export class BuyerController {
+
+    @Post(':id')
+    public async offerPrice(@Param('id') id: string, @Body() body: Price) {
+        return body;
+    }
+}
