955d3587d3d647ab9cb0c4fa8ed406c151896185
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-04 15:44:13 +0700

    :sparkles: Add request for buyer to submit quantity

--- fabric-server/src/buyer/buyer.dto.ts
+++ fabric-server/src/buyer/buyer.dto.ts
@@ -1,6 +1,12 @@
-import {IsPositive} from 'class-validator';
+import { IsPositive } from 'class-validator';
 
 export class Price {
     @IsPositive()
     price: number;
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class Quantity {
+    @IsPositive()
+    quantity: number;
 }
