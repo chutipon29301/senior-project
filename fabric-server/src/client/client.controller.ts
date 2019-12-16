b347304c0211de6ec3a897f6d51f948b879f8b42
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-16 21:56:49 +0700

    :sparkles: Add getClientForOrg function

--- fabric-server/src/client/client.controller.ts
+++ fabric-server/src/client/client.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Param } from '@nestjs/common';
+import { ClientService } from './client.service';
+import * as Client from 'fabric-client';
 
 @Controller('client')
-export class ClientController {}
+export class ClientController {
+
+    constructor(private readonly service: ClientService) {}
+
+    @Get('org/:orgName')
+    public async getClientForOrg(@Param('orgName') orgName: string): Promise<string> {
+        const client = await this.service.getClientForOrg(orgName);
+        return client.getMspid();
+    }
+}
