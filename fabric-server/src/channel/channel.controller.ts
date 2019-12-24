26abe0978c5d476e225b13413bf505d8c8c9a26a
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-24 22:22:46 +0700

    :sparkles: Add get channel request

--- fabric-server/src/channel/channel.controller.ts
+++ fabric-server/src/channel/channel.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { CreateChannelDto } from './channel.dto';
 
 @Controller('channel')
 export class ChannelController {
-
     constructor(private readonly channelService: ChannelService) {}
-
+    @Get()
+    public async getChannels() {
+        return this.channelService.getChannels();
+    }
     @Post()
     public async createChannel(@Body() body: CreateChannelDto) {
         return this.channelService.createChannel(body.channelName, body.orgName);
     }
+
 }
--- fabric-server/src/channel/channel.service.ts
+++ fabric-server/src/channel/channel.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ClientService } from '../client/client.service';
 import { readFileSync } from 'fs';
+import { ChannelRequest, ChannelInfo } from 'fabric-client';
 
 @Injectable()
 export class ChannelService {
@@ -12,32 +13,38 @@ export class ChannelService {
         const envelope = readFileSync('/home/fabric/config/channel.tx');
         const channelConfig = client.extractChannelConfig(envelope);
         const signature = client.signChannelConfig(channelConfig);
+        const orderer = client.getOrderer('orderer.example.com');
         // const request = {
         //     config: channelConfig,
         //     signatures: [signature],
         //     name: channelName,
         //     txId: client.newTransactionID(true),
         // };
-        const result = await client.createChannel({
+        console.log(orderer);
+        console.log(orderer.getName());
+        const request = {
             config: channelConfig,
             signatures: [signature],
             name: channelName,
             envelope,
             txId: client.newTransactionID(true),
-            orderer: client.getOrderer('orderer.example.com'),
-        });
+            // orderer,
+        } as ChannelRequest;
+        // console.log(request);
+        const result = await client.createChannel(request);
+        console.log('Hellooooo');
 
         if (result) {
             if (result.status === 'SUCCESS') {
                 const response = {
                     success: true,
-                    message: 'Channel \'' + channelName + '\' created Successfully'
+                    message: 'Channel \'' + channelName + '\' created Successfully',
                 };
                 return response;
             } else {
                 const response = {
                     success: false,
-                    message: 'Channel \'' + channelName + '\' failed to create status:' + result.status + ' reason:' + result.info
+                    message: 'Channel \'' + channelName + '\' failed to create status:' + result.status + ' reason:' + result.info,
                 };
                 return response;
             }
@@ -48,5 +55,11 @@ export class ChannelService {
             };
             return response;
         }
+    }
+
+    public async getChannels(): Promise<ChannelInfo[]> {
+        const client = await this.clientService.getClientForOrg('org1', 'peer0.org1.example.com');
+        const response = await client.queryChannels('peer0.org1.example.com');
+        return response.channels;
     }
 }
