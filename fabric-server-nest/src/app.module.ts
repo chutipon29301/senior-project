cc038b918446786f7311e9448360a281c5564167
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-25 22:16:05 +0700

    :sparkles: Add function to register user

--- fabric-server-nest/src/app.module.ts
+++ fabric-server-nest/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
