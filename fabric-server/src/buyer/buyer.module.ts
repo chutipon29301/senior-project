0f250637fbfc41bf3b454a4d92d10dc4e82e1714
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-25 13:44:14 +0700

    :fire: Remove fabric related files

--- fabric-server/src/buyer/buyer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BuyerService } from './buyer.service';
-import { BuyerController } from './buyer.controller';
-
-@Module({
-  providers: [BuyerService],
-  controllers: [BuyerController],
-})
-export class BuyerModule {}
