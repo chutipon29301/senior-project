0e40c6dec493e350bcd3cbe07158517f94e5b757
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-25 23:21:53 +0700

    :sparkles: Init chaincode module

--- fabric-server-nest/src/app.module.ts
+++ fabric-server-nest/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ClientModule } from './client/client.module';
 import { ChannelModule } from './channel/channel.module';
+import { ChaincodeModule } from './chaincode/chaincode.module';
 
 @Module({
-  imports: [UserModule, ClientModule, ChannelModule],
+  imports: [UserModule, ClientModule, ChannelModule, ChaincodeModule],
   controllers: [AppController],
   providers: [AppService],
 })
