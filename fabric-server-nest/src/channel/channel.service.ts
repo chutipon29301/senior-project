a95868a9d71cdea62eddcbb300a453e0aabb7f6e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-25 23:20:36 +0700

    :sparkles: Add function to update anchor peer

--- fabric-server-nest/src/channel/channel.service.ts
+++ fabric-server-nest/src/channel/channel.service.ts
@@ -2,6 +2,7 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { ClientService } from '../client/client.service';
 import { readFileSync } from 'fs';
 import { ChannelRequest } from 'fabric-client';
+import { join } from 'path';
 @Injectable()
 export class ChannelService {
 
@@ -140,5 +141,113 @@ export class ChannelService {
             };
             return response;
         }
+    }
+
+    public async updateAnchorPeers(channelName: string, configUpdatePath: string, username: string, orgName: string) {
+        console.log(`\n====== Updating Anchor Peers on '${channelName}' ======\n`);
+        let errorMessage = null;
+        try {
+            // first setup the client for this org
+            const client = await this.clientService.getClientForOrg(orgName, username);
+            console.log(`Successfully got the fabric client for the organization '${orgName}'`);
+            const channel = client.getChannel(channelName);
+            if (!channel) {
+                const message = `Channel ${channelName} was not defined in the connection profile`;
+                console.error(message);
+                throw new Error(message);
+            }
+
+            // read in the envelope for the channel config raw bytes
+            const envelope = readFileSync(join('/home/server/artifacts/channel', configUpdatePath));
+            // extract the channel config bytes from the envelope to be signed
+            const channelConfig = client.extractChannelConfig(envelope);
+
+            // Acting as a client in the given organization provided with "orgName" param
+            // sign the channel config bytes as "endorsement", this is required by
+            // the orderer's channel creation policy
+            // this will use the admin identity assigned to the client when the connection profile was loaded
+            const signature = client.signChannelConfig(channelConfig);
+
+            const request = {
+                config: channelConfig,
+                signatures: [signature],
+                name: channelName,
+                txId: client.newTransactionID(true), // get an admin based transactionID
+            };
+
+            const promises = [];
+            const eventHubs = channel.getChannelEventHubsForOrg();
+            console.log(`found ${eventHubs.length} eventhubs for this organization ${orgName}`);
+            eventHubs.forEach((eh) => {
+                const anchorUpdateEventPromise = new Promise((resolve, reject) => {
+                    console.log('anchorUpdateEventPromise - setting up event');
+                    const eventTimeout = setTimeout(() => {
+                        const message = 'REQUEST_TIMEOUT:' + eh.getPeerAddr();
+                        console.log(message);
+                        eh.disconnect();
+                    }, 60000);
+                    eh.registerBlockEvent((block) => {
+                        console.log(`The config update has been committed on peer ${eh.getPeerAddr()}`);
+                        clearTimeout(eventTimeout);
+                        resolve();
+                    }, (err) => {
+                        clearTimeout(eventTimeout);
+                        console.log(err);
+                        reject(err);
+                    },
+                        // the default for 'unregister' is true for block listeners
+                        // so no real need to set here, however for 'disconnect'
+                        // the default is false as most event hubs are long running
+                        // in this use case we are using it only once
+                        { unregister: true, disconnect: true },
+                    );
+                    eh.connect();
+                });
+                promises.push(anchorUpdateEventPromise);
+            });
+
+            const sendPromise = client.updateChannel(request as ChannelRequest);
+            // put the send to the orderer last so that the events get registered and
+            // are ready for the orderering and committing
+            promises.push(sendPromise);
+            const results = await Promise.all(promises);
+            console.log(`------->>> R E S P O N S E :${results}`);
+            const response = results.pop(); //  orderer results are last in the results
+
+            if (response) {
+                if (response.status === 'SUCCESS') {
+                    console.log(`Successfully update anchor peers to the channel ${channelName}`);
+                } else {
+                    errorMessage = `Failed to update anchor peers to the channel
+                    ${channelName} with status: ${response.status} reason: ${response.info}`;
+                    console.log(errorMessage);
+                }
+            } else {
+                errorMessage = `Failed to update anchor peers to the channel ${channelName}`;
+                console.log(errorMessage);
+            }
+        } catch (error) {
+            console.error(error.toString());
+            throw new BadRequestException('Failed to update anchor peers due to error: ' + error.stack ? error.stack : error);
+        }
+
+        if (!errorMessage) {
+            const message = `Successfully update anchor peers in organization ${orgName} to the channel ${channelName}`;
+            console.log(message);
+            const response = {
+                success: true,
+                message,
+            };
+            return response;
+        } else {
+            const message = `Failed to update anchor peers. cause:${errorMessage}`;
+            console.log(message);
+            const response = {
+                success: false,
+                message,
+            };
+            return response;
+        }
+
     }
 }
