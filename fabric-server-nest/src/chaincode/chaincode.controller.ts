9e2c18de89f9d84e9aebac194fedb0e5262e695e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-25 23:53:48 +0700

    :sparkles: Add install chaincode function

--- fabric-server-nest/src/chaincode/chaincode.controller.ts
+++ fabric-server-nest/src/chaincode/chaincode.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { InstallChaincodeDto } from './chaincode.dto';
+import { ChaincodeService } from './chaincode.service';
 
 @Controller('chaincode')
-export class ChaincodeController {}
+export class ChaincodeController {
+
+    constructor(private readonly chaincodeService: ChaincodeService) { }
+
+    @Post()
+    public async installChaincode(@Body() body: InstallChaincodeDto) {
+        return this.chaincodeService.installChaincode(body.peers, body.username, body.orgName);
+    }
+}
--- fabric-server-nest/src/chaincode/chaincode.module.ts
+++ fabric-server-nest/src/chaincode/chaincode.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChaincodeService } from './chaincode.service';
 import { ChaincodeController } from './chaincode.controller';
+import { ClientModule } from '../client/client.module';
 
 @Module({
+  imports: [ClientModule],
   providers: [ChaincodeService],
-  controllers: [ChaincodeController]
+  controllers: [ChaincodeController],
 })
-export class ChaincodeModule {}
+export class ChaincodeModule { }
