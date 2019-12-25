989eccc3a39f5745f22eacab29bb4c7ba05b6cdb
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-12-25 23:20:36 +0700

    :sparkles: Add function to update anchor peer

--- fabric-server-nest/src/channel/channel.controller.ts
+++ fabric-server-nest/src/channel/channel.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
 import { ChannelService } from './channel.service';
-import { CreateChannelDto, JoinChannelDto } from './channel.dto';
+import { CreateChannelDto, JoinChannelDto, UpdateAnchorPeersDto } from './channel.dto';
 
 @Controller('channel')
 export class ChannelController {
@@ -15,5 +15,10 @@ export class ChannelController {
     @Post('join')
     public async joinChannel(@Body() body: JoinChannelDto) {
         return this.channelService.joinChannel(body.channelName, body.peers, body.username, body.orgName);
+    }
+
+    @Post(':channelName/anchorPeers')
+    public async updateAnchorPeers(@Param('channelName') channelName: string, @Body() body: UpdateAnchorPeersDto) {
+        return this.channelService.updateAnchorPeers(channelName, body.configUpdatePath, body.username, body.orgName);
     }
 }
--- fabric-server-nest/src/channel/channel.dto.ts
+++ fabric-server-nest/src/channel/channel.dto.ts
@@ -11,3 +11,10 @@ export class JoinChannelDto {
     username: string;
     orgName: string;
 }
+
+// tslint:disable-next-line: max-classes-per-file
+export class UpdateAnchorPeersDto {
+    configUpdatePath: string;
+    username: string;
+    orgName: string;
+}
