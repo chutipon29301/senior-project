3a5866f2136212d0b4f1fa9a6f3af510d05f8957
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-01 20:18:09 +0700

    :sparkles: Add APIs from express example

--- fabric-server-nest/src/client/client.service.ts
+++ fabric-server-nest/src/client/client.service.ts
@@ -1,11 +1,15 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, Inject } from '@nestjs/common';
 import * as Client from 'fabric-client';
+import { WINSTON } from '../constant';
+import { Logger } from 'winston';
 
 @Injectable()
 export class ClientService {
 
+    constructor(@Inject(WINSTON) private readonly logger: Logger) {}
+
     public async getClientForOrg(userOrg: string, username?: string): Promise<Client> {
-        console.log(`getClientForOrg - ****** START ${userOrg} ${username}`)
+        this.logger.debug(`getClientForOrg - ****** START ${userOrg} ${username}`)
         const client = Client.loadFromConfig('/home/server/artifacts/network-config.yaml');
         client.loadFromConfig(`/home/server/artifacts/${userOrg.toLowerCase()}.yaml`);
         await client.initCredentialStores();
@@ -14,10 +18,10 @@ export class ClientService {
             if (!user) {
                 throw new BadRequestException(`User was not found : ${username}`);
             } else {
-                console.log('User %s was found to be registered and enrolled', username);
+                this.logger.debug('User %s was found to be registered and enrolled', username);
             }
         }
-        console.log('getClientForOrg - ****** END %s %s \n\n', userOrg, username);
+        this.logger.debug('getClientForOrg - ****** END %s %s \n\n', userOrg, username);
         return client;
     }
 
