3a962d033a778673b491f3fc0f1686641c1703d8
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-02 15:28:59 +0700

    :heavy_plus_sign: Add typeorm

--- trading/src/app.module.ts
+++ trading/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.mySQLUrl,
+      port: +configService.mySQLPort,
+      username: configService.mySQLUsername,
+      password: configService.mySQLPassword,
+      database: configService.mySQLDatabaseName,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  }), ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
