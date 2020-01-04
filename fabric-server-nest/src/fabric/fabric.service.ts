c4f6ac1a08392603be0db91e4f7ae2c9faa29df6
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-04 22:04:35 +0700

    :recycle: Move user to fabric

--- fabric-server-nest/src/fabric/fabric.service.ts
+++ fabric-server-nest/src/fabric/fabric.service.ts
@@ -11,6 +11,26 @@ export class FabricService {
 
     constructor(@Inject(WINSTON) private readonly logger: Logger) { }
 
+    public async findOneOrCreate(username: string, organizationName: string) {
+        const client = await this.getClientForOrg(organizationName);
+        this.logger.info('Successfully initialized the credential stores');
+        let user = await client.getUserContext(username, true);
+        if (!(user && user.isEnrolled())) {
+            // Create new user if not enrolled
+            this.logger.debug(`User ${username} was not enrolled, so we will need an admin user object to register`);
+            const adminUser = await client.setUserContext({ username: 'admin', password: 'adminpw' });
+            const caClient = client.getCertificateAuthority();
+            const secret = await caClient.register({
+                enrollmentID: username,
+                affiliation: `${organizationName.toLowerCase()}.department1`,
+            }, adminUser);
+            user = await client.setUserContext({ username, password: secret });
+        }
+        if (!(user && user.isEnrolled())) {
+            throw new BadRequestException('User was not enrolled');
+        }
+    }
+
     public async createChannel(channelName: string, username: string, organizationName: string) {
         this.logger.info(`====== Creating Channel '${channelName}' ======`);
         // Setup client for organization
