5338604197ff4dade950fc9c9ac022237516200c
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-04 22:50:21 +0700

    :sparkles: Add used function to chaincode file

--- fabric-server-nest/artifacts/src/github.com/example_cc/node/example_cc.js
+++ fabric-server-nest/artifacts/src/github.com/example_cc/node/example_cc.js
@@ -13,39 +13,10 @@ var Chaincode = class {
   async Init(stub) {
     console.info('========= example_cc Init =========');
     return shim.success();
-    // let ret = stub.getFunctionAndParameters();
-    // console.info(ret);
-    // let args = ret.params;
-    // // initialise only if 4 parameters passed.
-    // if (args.length != 4) {
-    //   return shim.error('Incorrect number of arguments. Expecting 4');
-    // }
-
-    // let A = args[0];
-    // let B = args[2];
-    // let Aval = args[1];
-    // let Bval = args[3];
-
-    // if (typeof parseInt(Aval) !== 'number' || typeof parseInt(Bval) !== 'number') {
-    //   return shim.error('Expecting integer value for asset holding');
-    // }
-
-    // try {
-    //   await stub.putState(A, Buffer.from(Aval));
-    //   try {
-    //     await stub.putState(B, Buffer.from(Bval));
-    //     return shim.success();
-    //   } catch (err) {
-    //     return shim.error(err);
-    //   }
-    // } catch (err) {
-    //   return shim.error(err);
-    // }
   }
 
   async Invoke(stub) {
     console.info('========= example_cc Invoke =========');
-    // return shim.success(Buffer.from('Hello'));
     let ret = stub.getFunctionAndParameters();
     console.info(ret);
     let method = this[ret.fcn];
@@ -66,96 +37,66 @@ var Chaincode = class {
   }
 
   async createRound(stub, args) {
-    let id = args[0];
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+    const id = args[0];
+    const modifyDate = args[1];
     await stub.putState(id, Buffer.from(JSON.stringify({
       buy: [],
       sell: [],
       result: [],
-      modifyDate: '',
+      modifyDate,
     })));
   }
 
+  async addSellerBid(stub, args) {
+    if (args.length != 4) {
+      throw new Error('Incorrect number of arguments. Expecting 4');
+    }
+    const roundId = args[0];
+    const sellerId = args[1];
+    const price = args[2];
+    const timestamp = args[3];
+    const result = await stub.getState(roundId);
+    if (!result) {
+      throw new Error(`Result with id ${roundId} not found`);
+    }
+    const resultObject = JSON.parse(result.toString());
+    resultObject.sell.push({
+      id: sellerId,
+      price,
+      timestamp,
+    });
+    await stub.putState(roundId, Buffer.from(JSON.stringify(resultObject)));
+  }
+
+  async addBuyerBid(stub, args) {
+    if (args.length != 4) {
+      throw new Error('Incorrect number of arguments. Expecting 4');
+    }
+    const roundId = args[0];
+    const buyerId = args[1];
+    const price = args[2];
+    const timestamp = args[3];
+    const result = await stub.getState(roundId);
+    if (!result) {
+      throw new Error(`Result with id ${roundId} not found`);
+    }
+    const resultObject = JSON.parse(result.toString());
+    resultObject.buy.push({
+      id: buyerId,
+      price,
+      timestamp,
+    });
+    await stub.putState(roundId, Buffer.from(JSON.stringify(resultObject)));
+  }
+
   async getRound(stub, args) {
-    // return Buffer.from('Hello');
     let id = args[0];
     let result = await stub.getState(id);
     return result;
   }
-
-  // async move(stub, args) {
-  //   if (args.length != 3) {
-  //     throw new Error('Incorrect number of arguments. Expecting 3');
-  //   }
-
-  //   let A = args[0];
-  //   let B = args[1];
-  //   if (!A || !B) {
-  //     throw new Error('asset holding must not be empty');
-  //   }
-
-  //   // Get the state from the ledger
-  //   let Avalbytes = await stub.getState(A);
-  //   if (!Avalbytes) {
-  //     throw new Error('Failed to get state of asset holder A');
-  //   }
-  //   let Aval = parseInt(Avalbytes.toString());
-
-  //   let Bvalbytes = await stub.getState(B);
-  //   if (!Bvalbytes) {
-  //     throw new Error('Failed to get state of asset holder B');
-  //   }
-
-  //   let Bval = parseInt(Bvalbytes.toString());
-  //   // Perform the execution
-  //   let amount = parseInt(args[2]);
-  //   if (typeof amount !== 'number') {
-  //     throw new Error('Expecting integer value for amount to be transaferred');
-  //   }
-
-  //   Aval = Aval - amount;
-  //   Bval = Bval + amount;
-  //   console.info(util.format('Aval = %d, Bval = %d\n', Aval, Bval));
-
-  //   // Write the states back to the ledger
-  //   await stub.putState(A, Buffer.from(Aval.toString()));
-  //   await stub.putState(B, Buffer.from(Bval.toString()));
-
-  // }
-
-  // // Deletes an entity from state
-  // async delete(stub, args) {
-  //   if (args.length != 1) {
-  //     throw new Error('Incorrect number of arguments. Expecting 1');
-  //   }
-
-  //   let A = args[0];
-
-  //   // Delete the key from the state in ledger
-  //   await stub.deleteState(A);
-  // }
-
-  // // query callback representing the query of a chaincode
-  // async query(stub, args) {
-  //   if (args.length != 1) {
-  //     throw new Error('Incorrect number of arguments. Expecting name of the person to query')
-  //   }
-
-  //   let jsonResp = {};
-  //   let A = args[0];
-
-  //   // Get the state from the ledger
-  //   let Avalbytes = await stub.getState(A);
-  //   if (!Avalbytes) {
-  //     jsonResp.error = 'Failed to get state for ' + A;
-  //     throw new Error(JSON.stringify(jsonResp));
-  //   }
-
-  //   jsonResp.name = A;
-  //   jsonResp.amount = Avalbytes.toString();
-  //   console.info('Query Response:');
-  //   console.info(jsonResp);
-  //   return Avalbytes;
-  // }
 };
 
 shim.start(new Chaincode());
