ca1e615bb77448d603e949b00070064d8dca8b9a
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-06 22:49:15 +0700

    :sparkles: Generate config for 1 peer per 1 org

---
+++ fabric-server-nest/artifacts/channel/generate.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../../../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+CHANNEL_NAME=mychannel
+
+# remove previous crypto material and config transactions
+# rm -fr config/*
+rm -fr crypto-config/*
+
+# generate crypto material
+cryptogen generate --config=./cryptogen.yaml
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate crypto material..."
+  exit 1
+fi
+
+# generate genesis block for orderer
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
+
+# generate channel configuration transaction
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org1MSP..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org2MSP..."
+  exit 1
+fi
--- fabric-server-nest/artifacts/src/github.com/example_cc/node/example_cc.js
+++ fabric-server-nest/artifacts/src/github.com/example_cc/node/example_cc.js
@@ -46,7 +46,7 @@ var Chaincode = class {
       buy: [],
       sell: [],
       result: [],
-      modifyDate,
+      modifyDate: '',
     })));
   }
 
