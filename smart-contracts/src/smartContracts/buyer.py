77df9d1eb42f4869d8ec89045cf39e716124f922
mingyanisa <mingignim@hotmail.com>
2020-01-08 10:37:28 +0700

    :sparkles: adding timestamp sorting and edit evaluation matrix

--- smart-contracts/src/smartContracts/buyer.py
+++ smart-contracts/src/smartContracts/buyer.py
@@ -2,18 +2,22 @@ class Buyer:
     quantityWant = 0
     quantityLeft = 0
     bidPrice = 0
+    isGrid=False
     transaction =[]
+    timestamp=None
     totalBoughtPrice = 0
     bsi=0;mti=0
     utilityIndex=0
     # default constructor 
-    def __init__(self,quantityWant,bidPrice,isGrid=False): 
+    def __init__(self,quantityWant,bidPrice,timestamp): 
         self.quantityWant = quantityWant
-        if(isGrid):self.quantityLeft = 0
-        else:self.quantityLeft = quantityWant
+        self.quantityLeft = quantityWant
+        self.timestamp=timestamp
+        self.bsi = 0
+        self.isGrid=False
         self.bidPrice = bidPrice
         self.totalBoughtPrice = 0
-        self.bsi = 0;self.mti=0
+        self.mti=0
         self.utilityIndex=0
         self.transaction=[]
   
@@ -25,8 +29,14 @@ class Buyer:
         "\nbuyer bought transaction:",self.transaction,
         "\nbuyer totalBoughtPrice price:",self.totalBoughtPrice,
          "\nbsi:",self.bsi,
-         "\nmti:",self.mti
+         "\nmti:",self.mti,
+        "\nisGrid:",self.isGrid
         ) 
         # print("====")
-    def to_dict(self):
+    def toDict(self):
       return {"transaction": self.transaction, "totalBoughtPrice": self.totalBoughtPrice}
+    def toGrid(self):
+        self.isGrid=True
+        self.bsi=1
+        self.quantityLeft = 0
+        return self
--- smart-contracts/src/smartContracts/seller.py
+++ smart-contracts/src/smartContracts/seller.py
@@ -3,19 +3,23 @@ class Seller:
     quantityLeft = 0
     reservePrice = 0
     transaction=[]
+    timestamp=None
+    isGrid=False
     totalSoldPrice=0
     utilityIndex=0
     ssi=0;mti=0
     # default constructor 
-    def __init__(self,quantityAvailable,reservePrice,isGrid=False): 
+    def __init__(self,quantityAvailable,reservePrice,timestamp): 
         self.quantityAvailable = quantityAvailable
-        if(isGrid):self.quantityLeft = 0
-        else:self.quantityLeft = quantityAvailable
+        self.quantityLeft = quantityAvailable
+        self.timestamp=timestamp
+        self.ssi=0
+        self.isGrid=False
         self.reservePrice = reservePrice
         self.transaction=[]
         self.totalSoldPrice=0
         self.utilityIndex=0
-        self.ssi=0;self.mti=0
+        self.mti=0
   
     # a method for printing data members 
     def print_seller(self): 
@@ -26,8 +30,14 @@ class Seller:
         "\nseller totalSoldPrice price:",self.totalSoldPrice, 
         "\nssi:",self.ssi,
         "\nmti:",self.mti,
+        "\nisGrid:",self.isGrid
         ) 
         # print("====")
         
     def toDict(self):
       return {"transaction": self.transaction, "totalSoldPrice": self.totalSoldPrice}
+    def toGrid(self):
+        self.isGrid=True
+        self.quantityLeft = 0
+        self.ssi=1
+        return self
