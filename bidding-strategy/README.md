cc96fbb82e92f7b019cb7e8a66d345709d85b6f3
mingyanisa <mingignim@hotmail.com>
2020-01-13 14:02:48 +0700

    :sparkles: Add bidding strategy server

---
+++ bidding-strategy/README.md
@@ -0,0 +1,5 @@
+# Bidding strategy
+
+Activate env
+
+` pytest --pyargs src `
---
+++ bidding-strategy/src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request,jsonify,json
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+import urllib.parse
+import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+
