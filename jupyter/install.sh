8ba0d84497d01b6837844626d209d8019f0c797e
mingyanisa <mingignim@hotmail.com>
2020-01-15 23:49:28 +0700

    :whale: edit dockerfile for dependencies installation

--- jupyter/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get install -y cmake libopenmpi-dev python3-dev zlib1g-dev
-pip install --requirement requirements.txt
-pip install stable-baselines 
-pip install git+https://github.com/zhy0/dmarket_rl
-pip install -r ./requirements.txt
