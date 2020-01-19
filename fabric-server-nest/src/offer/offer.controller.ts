cb586415c8a0b7dcfbfcfb857cfadfb60618e493
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-19 18:13:09 +0700

    :sparkles: Add request to offer module

--- fabric-server-nest/src/offer/offer.controller.ts
+++ fabric-server-nest/src/offer/offer.controller.ts
@@ -1,18 +1,27 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { OfferDto } from './offer.dto';
+import { CreateOfferDto } from './offer.dto';
 import { OfferService } from './offer.service';
+import { Orgs } from '../decorator/org.decorator';
+import User, { Organization } from '../entity/User.entity';
+import { RequestUser } from '../decorator/user.decorator';
 
 @Controller('offer')
 export class OfferController {
 
     constructor(private readonly service: OfferService) { }
 
-    @Post('pv')
-    public async createSellerOffer(@Body() { id, price }: OfferDto) {
-        this.service.sendPvOffer(id, price);
+    @Orgs(Organization.Building, Organization.PV)
+    @Post('')
+    public async createOffer(@RequestUser() user: User, @Body() { price, roundId }: CreateOfferDto) {
+        
     }
-    @Post('building')
-    public async createBuyerOffer(@Body() { id, price }: OfferDto) {
-        this.service.sendBuildingOffer(id, price);
-    }
+
+    // @Post('pv')
+    // public async createSellerOffer(@Body() { id, price }: OfferDto) {
+    //     this.service.sendPvOffer(id, price);
+    // }
+    // @Post('building')
+    // public async createBuyerOffer(@Body() { id, price }: OfferDto) {
+    //     this.service.sendBuildingOffer(id, price);
+    // }
 }
--- fabric-server-nest/src/offer/offer.service.ts
+++ fabric-server-nest/src/offer/offer.service.ts
@@ -1,15 +1,22 @@
-import { Injectable, HttpService } from '@nestjs/common';
+import { Injectable, HttpService, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '../config/config.service';
+import { FabricService } from '../fabric/fabric.service';
+import { Organization } from '../entity/User.entity';
 
 @Injectable()
 export class OfferService {
     constructor(
-        private readonly httpService: HttpService,
+        private readonly fabricService: FabricService,
     ) { }
-    public async sendBuildingOffer(id: string, price: number) {
-;
-    }
-    public async sendPvOffer(id: string, price: number) {
 
+    public async createOffer(roundId: string, price: number, organization: Organization, username: string) {
+        switch (organization) {
+            case Organization.Building:
+                return this.fabricService.addBuyerBid(roundId, price, organization, username);
+            case Organization.PV:
+                return this.fabricService.addSellerBid(roundId, price, organization, username);
+            case Organization.Utility:
+                throw new UnauthorizedException();
+        }
     }
 }
