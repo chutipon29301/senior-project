4da8a3291a15429ecb079fe13be699c234f21ca6
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-01-20 22:53:07 +0700

    :fire: remove fabric-temp

--- fabric-server-nest/src/fabric-temp/fabric-temp.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FabricTempService } from './fabric-temp.service';
-import { FabricTempController } from './fabric-temp.controller';
-
-@Module({
-  providers: [FabricTempService],
-  controllers: [FabricTempController],
-})
-export class FabricTempModule { }
