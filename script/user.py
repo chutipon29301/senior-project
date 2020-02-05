59387868ced35032db0a56958c142cb5454f21dc
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-02-05 09:33:44 +0700

    :sparkles: Add smart meter id to create user script

--- script/user.py
+++ script/user.py
@@ -16,13 +16,13 @@ class User():
         self.USER_LIST = os.getenv('USER_LIST')
         self.USER_STORAGE = os.getenv('USER_STORAGE')
         if not os.path.isfile(self.USER_STORAGE):
-            columns = ['name', 'id', 'token', 'organization']
+            columns = ['name', 'organization', 'id', 'smartMeterId', 'token']
             self.userDataframe = pd.DataFrame(columns=columns)
         else:
             self.userDataframe = pd.read_csv(self.USER_STORAGE)
 
-    def create(self, name, org):
-        userInfo = asyncio.run(self.asyncCreateUserAndToken(name, org))
+    def create(self, name, org, smartMeterId):
+        userInfo = asyncio.run(self.asyncCreateUserAndToken(name, org, smartMeterId))
         self.userDataframe = self.userDataframe.append(
             userInfo, ignore_index=True)
         self.userDataframe.to_csv(self.USER_STORAGE, index=False)
@@ -34,7 +34,7 @@ class User():
         bar = ChargingBar('Creating users', max=len(df.index),
                           suffix='%(percent)d%%; Elapsed: %(elapsed)ds; ETA: %(eta)ds')
         for index, row in df.iterrows():
-            userInfo = asyncio.run(self.asyncCreateUserAndToken(row['name'], row['organization']))
+            userInfo = asyncio.run(self.asyncCreateUserAndToken(row['name'], row['organization'], row['smartMeterId']))
             self.userDataframe = self.userDataframe.append(
                 userInfo, ignore_index=True)
             bar.next()
@@ -66,9 +66,9 @@ class User():
         self.userDataframe.to_csv(self.USER_STORAGE, index=False)
         bar.finish()
 
-    async def asyncCreateUserAndToken(self, username, organization):
+    async def asyncCreateUserAndToken(self, username, organization, smartMeterId):
         async with aiohttp.ClientSession() as session:
-            userResponse = await self.asyncCreateUser(username, organization, session)
+            userResponse = await self.asyncCreateUser(username, organization, smartMeterId, session)
             tokenResponse = await self.asyncGetToken(userResponse['id'], session)
             return {
                 'name': userResponse['name'],
@@ -77,13 +77,14 @@ class User():
                 'token': tokenResponse['token']
             }
 
-    async def asyncCreateUser(self, username, organization, session=None):
+    async def asyncCreateUser(self, username, organization, smartMeterId, session=None):
         async def createUser(session, username, organization):
             async with session.post(
                     urllib.parse.urljoin(self.BASE_URL, 'user'),
                     data={
                         "name": username,
-                        "organizationName": organization
+                        "organizationName": organization,
+                        "smartMeterId": smartMeterId
                     }) as response:
                 return await response.json()
 
