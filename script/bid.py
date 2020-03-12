398382f19b5cb16d1b28bd8b4bb408a6aa84bbe2
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2020-03-12 20:05:18 +0700

    :sparkles: Complete bidding function

--- script/bid.py
+++ script/bid.py
@@ -4,46 +4,53 @@ import requests
 import urllib.parse
 import os 
 import pandas as pd 
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+from progress.bar import ChargingBar
 class Bid():
 
     def __init__(self):
         self.USER_STORAGE = os.getenv('USER_STORAGE')
-    # def ping(self):
-    #     return 'pong'
-    # def createOffer(self,token):
-    #     token="53b0d964-f57f-4432-a86f-7fc067438f4a,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzYjBkOTY0LWY1N2YtNDQzMi1hODZmLTdmYzA2NzQzOGY0YSIsIm5hbWUiOiJDaGFtMSIsIm9yZ2FuaXphdGlvbiI6IkJ1aWxkaW5nIiwiaWF0IjoxNTgwNDA1Mjg2LCJleHAiOjE1ODEwMTAwODZ9.ARVr_46aHemkp-3-QVw2T2-6rrLiK8R9yCDz_2P1BUA"
-    #     fake = faker.Faker()
-    #     rand_date = fake.date_time()  # datetime(2006, 4, 30, 3, 1, 38)
-    #     rand_date_iso = rand_date.isoformat()  # '2006-04-30T03:01:38'
-    #     # res = requests.post(
-    #     #     urllib.parse.urljoin(os.getenv('FABRIC_URL'),
-    #     #                          '/offer/createOffer'),
-    #     #     data={
-    #     #         'price': random.uniform(0,5),
-    #     #         'date': rand_date_iso
-    #     #     },
-    #     #     headers={
-    #     #         'authorization': "Bearer "+ token
-    #     #     }
-    #     # )
-    #     return 'res'
 
-        # self.USER_PATH = os.path.join(os.getenv('STORAGE_DIR'), 'user.csv')
-    def createOfferFrom(self,step=10):
-        timestamp=datetime.now()
-        for i in range(step):
-            timestamp=timestamp+timedelta(hours=1)
-        self.createAllOffer(timestamp)
+    def createOfferFromNow(self, round = 10):
+        timestamp=datetime.utcnow()
+        bar = ChargingBar('Creating offers', max=round, suffix='%(percent)d%%; Elapsed: %(elapsed)ds; ETA: %(eta)ds')
+        for i in range(round):
+            timestamp=timestamp+timedelta(hours = 1)
+            self.createAllOffer(timestamp)
+            bar.next()
+        bar.finish()
 
-    def createAllOffer(self,timestamp):
+    def createAllOffer(self, timestamp):
         df = pd.read_csv(self.USER_STORAGE)
+        df=df[df.organization != 'Utility']
         tokens = df['token'].to_list()
         for token in tokens:
             self.createOffer(token,timestamp)
-        
 
-    def createOffer(self,token,timestamp):
+    def clearMarket(self):
+        df = pd.read_csv(self.USER_STORAGE)
+        token = df[df['organization'] != 'Utility']['token'].to_list()[0]
+        rounds = requests.get(
+            urllib.parse.urljoin(os.getenv('FABRIC_URL'), '/round'),
+            headers = {
+                'authorization': "Bearer "+ token
+            }
+        )
+        bar = ChargingBar('Clearing Market', max=len(rounds.json()), suffix='%(percent)d%%; Elapsed: %(elapsed)ds; ETA: %(eta)ds')
+        for round in rounds.json():
+            response = requests.post(
+                urllib.parse.urljoin(os.getenv('FABRIC_URL'), '/round/clearMarket'),
+                headers = {
+                    'authorization': "Bearer "+ token
+                },
+                json = {
+                    'roundId': round['id']
+                }
+            )
+            bar.next()
+        bar.finish()
+
+    def createOffer(self, token, timestamp):
         body = {
             'price': random.uniform(0, 5),
             'date': timestamp.isoformat()
@@ -55,5 +62,3 @@ class Bid():
             },
             json = body
         )
-        print(res.status_code)
-        
